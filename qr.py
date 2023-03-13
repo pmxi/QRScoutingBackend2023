b3003a59b63d4c8f499288cada336bc464f7bf65
Paras M <64939659+pmxi@users.noreply.github.com>
2023-03-12 23:07:36 -0400

    working version

--- qr.py
+++ qr.py
@@ -5,71 +5,61 @@ import cv2
 import tomllib
 
 parser = argparse.ArgumentParser(description="qr code scanner to text file")
-# parser.add_argument("-f", "--file", type=str, default="scouting.txt",
-#                     help="file to output qr code scans to, relative or absolute")
 parser.add_argument("-v", "--verbose", action="store_true")
-parser.add_argument("-d", "--remove-duplicate", action="store_true")
 args = parser.parse_args()
 
 
-def decoder(image):
-    gray_img = cv2.cvtColor(image, 0)
-    barcode = decode(gray_img)
-
-    for obj in barcode:
-        points = obj.polygon
-        (x, y, w, h) = obj.rect
-        pts = np.array(points, np.int32)
-        pts = pts.reshape((-1, 1, 2))
-        cv2.polylines(image, [pts], True, (0, 255, 0), 3)
-        barcode_data = obj.data.decode("utf-8")
-        v.add(barcode_data)
-        barcode_type = obj.type
-        string = str(barcode_data) + " | Type " + str(barcode_type)
-        cv2.putText(
-            frame, string, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2
-        )
-        if args.verbose:
-            print("QR: " + barcode_data + " | Type: " + barcode_type)
+# def save(values):
+#     with open(args.file, "a") as output_file:
+#         if len(v) != 0:
+#             for i in values:
+#                 output_file.write(i + "\n")
+#             print("SAVED! ( ͡° ͜ʖ ͡°)\n")
+#         else:
+#             print("SAVED NOTHING! ¯\\_(ツ)_/¯\n")
 
 
-def save(values):
-    with open(args.file, "a") as output_file:
-        if len(v) != 0:
-            for i in values:
-                output_file.write(i + "\n")
-            print("SAVED! ( ͡° ͜ʖ ͡°)\n")
-        else:
-            print("SAVED NOTHING! ¯\\_(ツ)_/¯\n")
+def main():
+    with open("config.toml", "rb") as f:
+        config = tomllib.load(f)
+    # data = pd.read_csv(config["output_file"], sep='\t')
+    cap = cv2.VideoCapture(config["camera_id"])
+    data_submitted = []
+    while True:
+        ret, frame = cap.read()
+        gray_img = cv2.cvtColor(frame, 0)
+        barcode = decode(gray_img)
+        for obj in barcode:
+            points = obj.polygon
+            (x, y, w, h) = obj.rect
+            pts = np.array(points, np.int32)
+            pts = pts.reshape((-1, 1, 2))
+            cv2.polylines(frame, [pts], True, (0, 255, 0), 3)
+            barcode_data = obj.data.decode("utf-8")
+            barcode_type = obj.type
+            string = str(barcode_data) + " | Type " + str(obj.type)
+            cv2.putText(
+                frame, string, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2
+            )
+            if args.verbose:
+                print("QR: " + barcode_data + " | Type: " + barcode_type)
+            if barcode_data not in data_submitted:
+                data_submitted.append(barcode_data)
+                with open(config["output_file"], "a") as output_file:
+                    output_file.write(barcode_data + "\n")
+                print("QR:" + barcode_data)
+                print("SAVED! ( ͡° ͜ʖ ͡°)")
+
+        cv2.imshow("QR SCANNER", frame)
+        code = cv2.waitKey(10)
+        if code == ord("s"):
+            pass
+        elif code == ord("p"):
+            for data in data_submitted:
+                print(data)
+        elif code == ord("q"):
+            break
 
 
 if __name__ == "__main__":
-    with open("config.toml", "rb") as f:
-        config = tomllib.load(f)
-
-    if not args.remove_duplicate:
-        cap = cv2.VideoCapture(0)
-        v = set()
-        while True:
-            ret, frame = cap.read()
-            decoder(frame)
-            cv2.imshow("QR SCANNER", frame)
-            code = cv2.waitKey(10)
-            if code == ord("s"):
-                print("Saving! (O.o)\n         /||\\\n         / \\")
-                save(v)
-                v.clear()
-            elif code == ord("p"):
-                for i in v:
-                    print(i)
-            elif code == ord("q"):
-                break
-
-    elif args.remove_duplicate:
-        with open(config["output_file"], "r+") as output_file:
-            values = set(output_file.readlines())
-            output_file.seek(0)
-            for v in values:
-                output_file.write(v)
-            output_file.truncate()
-        print("duplicates removed from scouting file: {args.files}")
+    main()
